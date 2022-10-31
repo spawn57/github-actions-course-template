eb60005cdb80e410471ba047667094c7554f3db2
Sunil Khiatani <sunil_k_khiatani@manulifeam.com>
2022-10-31 21:40:24 +0800

    fix tests

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -2,12 +2,9 @@ const Game = require('../src/game').default
 const fs = require('fs')
 
 describe('App', () => {
-  it('Contains the compiled JavaScript', async (done) => {
-    fs.readFile('./public/main.js', 'utf8', (err, data) => {
-      expect(err).toBe(null)
-      expect(data).toMatchSnapshot()
-      done()
-    })
+  it('Contains the compiled JavaScript', () => {
+    const data = fs.readFileSync('./public/main.js', 'utf8')
+    expect(data).toMatchSnapshot()
   })
 })
 
